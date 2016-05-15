834b710006e4bedf789abfdc78d369bb44dc7e48
Jimolle <jimolofsamuel@gmail.com>
2016-05-15 14:23:46 +0200

    Rotate- and some Offline/Online-js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,27 @@
 ﻿window.onload = function () {
-    /* 
-        Implementera kontroll av online och offline 
-    */   
+
+    function onlineOrOffline() {
+        var networkIco = document.getElementById("networkicon");
+
+        if (navigator.onLine) {
+            //alert("You are online!");
+            networkIco.style.fill = "green";
+        } else {
+            //alert("You are offline.");
+            networkIco.style.fill = "red";
+        }
+    }
+
+    onlineOrOffline();
+
+    // använd modernizer istället!?
+    if(window.addEventListener) {
+        window.addEventListener("offline", onlineOrOffline);
+        window.addEventListener("online", onlineOrOffline);
+    } else {
+        document.body.attachEvent("onoffline", onlineOrOffline);
+        document.body.attachEvent("ononline", onlineOrOffline);
+    }
+
 };
 
--- scripts/rotate.js
+++ scripts/rotate.js
@@ -1,6 +1,19 @@
-﻿function animateLoggo() {
-    /*
-        Implementera animeringen här..
-    */
-    alert("Implementation för animering saknas");
+﻿var logo, newDeg = 0, rotateYInterval;
+
+function animateLoggo()
+{
+    logo = document.getElementById("logo");
+    clearInterval(rotateYInterval);
+    rotateYInterval = setInterval("startYRotate()", 10);
+}
+
+function startYRotate()
+{
+    newDeg += 1;
+    logo.style.transform = "rotateY(" + newDeg + "deg)";
+    logo.style.webkitTransform = "rotateY(" + newDeg + "deg)";
+    logo.style.OTransform = "rotateY(" + newDeg + "deg)";
+    logo.style.MozTransform = "rotateY(" + newDeg + "deg)";
+    if (newDeg >= 360)
+        newDeg = 0;
 }
