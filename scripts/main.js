e5b4fa5894c0811e5fd98eab70b7d2e88b549711
Jimolle <jimolofsamuel@gmail.com>
2016-05-17 16:41:30 +0200

    Started implementing modernizr

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 ﻿window.onload = function () {
 
+    if (Modernizr.localstorage) {
+        alert("LocalStorage stödjs av din webbläsare!");
+        // run localStorage code here...
+    }
+    else {
+        // no native support for local storage
+        alert("Din webbläsare saknar stöd för localstorage!");
+        // TODO: Fallbacks osv osv, kör fler Modernizr också.
+    }
+
+
     function onlineOrOffline() {
         var networkIco = document.getElementById("networkicon");
 
@@ -18,10 +29,12 @@
         getLastSyncDate();
 
     // använd modernizer istället!?
-    if(window.addEventListener) {
+    if (Modernizr.eventlistener) {
+        alert("YES");
         window.addEventListener("offline", onlineOrOffline);
         window.addEventListener("online", onlineOrOffline);
     } else {
+        alert("NO");
         document.body.attachEvent("onoffline", onlineOrOffline);
         document.body.attachEvent("ononline", onlineOrOffline);
     }
